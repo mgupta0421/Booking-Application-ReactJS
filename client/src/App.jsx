48f4dd3f1d12da7fa4ff14154b903bd149f6d7b4
Mitali Gupta <mitaligupta726@gmail.com>
2024-01-18 15:45:04 -0500

    implement Login and Register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import IndexPage from "./pages/IndexPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
+import LoginPage from "./pages/LoginPage";
+import Layout from './Layout.jsx';
+import RegisterPage from "./pages/RegisterPage";
 
 
 function App(){
   return (
     <Routes>
-      <Route index element ={<IndexPage />}  />
-      <Route path="/login" element ={<LoginPage />} />
+      <Route path="/" element= {<Layout />}>
+        <Route index element ={<IndexPage />}  />
+        <Route path="/login" element ={<LoginPage />} />
+        <Route path="/register" element ={<RegisterPage />} />        
+      </Route>
+
+      
     </Routes>
     
   )
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,19 +1,19 @@
 
 import {Link} from "react-router-dom"
-export default function LoginPage(){
+export default function RegisterPage(){
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
-            <h1 className="text-4xl text-center mb-4"> Login</h1>
+            <h1 className="text-4xl text-center mb-4"> Register</h1>
             <form className="max-w-md mx-auto ">
                 <input type="email" placeholder={"your@email.com"} />
                 <input type="password" placeholder="password" />
-                <button> Login </button>
+                <button className="primary"> Register </button>
                 <div>
-                    Not a user yer ? <Link to={'/register'}> Register 
-                         </Link>
+                    Dont have a account ?
+                    <Link to ={'/login'}> Login
+                    </Link>
                 </div>
-
 
             </form>
             </div>
