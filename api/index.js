7920f89568426df40e611f277bcb288327416193
Mitali Gupta <mitaligupta726@gmail.com>
2024-01-31 18:16:19 -0500

    new client request added

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
+
 require('dotenv').config();
 const app = express();
 
@@ -77,6 +79,20 @@ app.get('/profile', (req,res) => {
     }
    res.json({token});
 });
+
+app.post('/logout', (req,res) => {
+    res.cookie('token', '').json(true);
+});
+
+app.post('/uploadByLink', async (req,res) => {
+        const {link} = req.body;
+        const newName = 'photo' + Date.now() + '.jpg';
+        await imageDownloader.image({
+            url: link,
+            dest: __dirname + '/uploads/',
+        });
+        res.json(newName);
+})
 
 app.listen(4000);
 
